3223ed8c02dd6dadec670384435476abc47eca41
Nemanja Stankovic <nemanjas0823@hotmail.com>
2025-02-03 16:39:38 -0500

    started app.py

--- server/app.py
+++ server/app.py
@@ -3,7 +3,7 @@
 # Standard library imports
 
 # Remote library imports
-from flask import request
+from flask import request, jsonify, session, make_response, render_template
 from flask_restful import Resource
 
 # Local imports
--- server/config.py
+++ server/config.py
@@ -7,7 +7,7 @@ from flask_migrate import Migrate
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
-
+import os
 # Local imports
 
 # Instantiate app, set attributes
