b4800cf45fbbf3f63701571d57cd609d9131788c
Nemanja Stankovic <nemanjas0823@hotmail.com>
2025-02-09 11:46:15 -0500

    added level and yen to header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,21 +2,60 @@ import NavBar from './NavBar';
 import Logo from './Logo';
 import ProfileDropdown from './ProfileDropdown';
 import styled from 'styled-components';
+import { useAuth } from '../context/AuthContext'
 
 const HeaderContainer = styled.header`
-  display: flex;
-  justify-content: space-between;
+display: flex;
   align-items: center;
   padding: 10px 20px;
   background-color: #151da6;
+  color: white;
+  position: relative;
+`;
+const LogoContainer = styled.div`
+  position: absolute;
+  left: 20px;
+`;
+const NavbarContainer = styled.div`
+  flex-grow: 1;
+  display: flex;
+  justify-content: center;
+`;
+const PlayerInfoContainer = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 12px;
+  position: absolute;
+  right: 160px;
+`;
+
+const InfoBox = styled.div`
+  background: #6d9ac7;
+  padding: 6px 12px;
+  border-radius: 8px;
+  text-align: center;
 `;
 const Header = () => {
-  
+  const { user } = useAuth();
+  const playerLevel = user?.level ?? 1;
+  const playerYen = user?.yen ?? 0;
 
 return(
 <HeaderContainer>
-      <Logo /> 
-      <NavBar /> 
+<LogoContainer>
+        <Logo />
+      </LogoContainer>
+      <NavbarContainer>
+        <NavBar />
+      </NavbarContainer>
+      <PlayerInfoContainer>
+        <InfoBox>
+         <h4>Level:{playerLevel}</h4>
+        </InfoBox>
+        <InfoBox>
+          <h4>¥ {playerYen}</h4>
+        </InfoBox>
+      </PlayerInfoContainer>
       <ProfileDropdown /> 
     </HeaderContainer>
   );
